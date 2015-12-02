8c63c2790cd0fcbb21e577a57c32387efec03c6a
Leigh <leight@gmail.com>
2015-12-02 15:02:02 +0000

    Fix issue with multiple small messages

--- lib/Cipher.php
+++ lib/Cipher.php
@@ -24,7 +24,7 @@ class Cipher
         $messageRemainder = $messageLen % 64;
         $blocks = ($messageLen >> 6) + ($messageRemainder > 0);
 
-        while ($blocks--) {
+        while ($blocks-- > 0) {
             list($k0, $k1, $k2, $k3, $k4, $k5, $k6, $k7, $k8, $k9, $k10, $k11, $k12, $k13, $k14, $k15) = $rk;
 
             $i = 10;
